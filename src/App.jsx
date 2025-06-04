67a3efb8e6e6cfd328a33cbc1a3d4d9553470b79
Jecinaha <hagg.jennifer@gmail.com>
2025-06-05 01:43:08 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import EventPage from './assets/pages/EventPage.jsx'
+import EventDetailsPage from './assets/pages/EventDetailsPage.jsx'
+import BookingEvent from './assets/pages/BookingEvent.jsx'
+
+function App() {
+
+
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<EventPage />} />
+      <Route path="/events/:id" element={<EventDetailsPage />} />
+      <Route path="/events/booking/:id" element={<BookingEvent />} />
+    </Routes>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/pages/EventPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Nav from '../Components/Nav'
+import Header from '../Components/Header'
+import Footer from '../Components/Footer'
+import EventList from '../Components/EventList'
+
+const EventPage = () => {
+  return (
+    <div className="portal-wrapper">
+        <Nav />
+        <Header />
+        <main>
+           <EventList />
+        </main>
+        <Footer /> 
+    </div>
+  )
+}
+
+export default EventPage
