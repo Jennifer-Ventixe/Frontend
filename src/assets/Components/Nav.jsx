2056eed9fd2168cb6d16f005a6dd4cd9ee4ce3ef
Jecinaha <hagg.jennifer@gmail.com>
2025-06-05 22:58:19 +0200

    fixat med layouten

--- src/assets/Components/Nav.jsx
+++ src/assets/Components/Nav.jsx
@@ -4,7 +4,35 @@ import { NavLink } from 'react-router-dom'
 const Nav = () => {
   return (
     <nav className="nav">
-        <NavLink to="/" >Events</NavLink>
+      <div className="nav-logo">
+        <img src="./Images/ventixe-logo.svg" alt="Ventixe logo" />
+        <h1>Ventixe</h1>
+      </div>
+      
+        <NavLink className="navlink" to="/" >
+        <img src="./Images/Events.svg" alt="Ticket" />
+        <p>Dashboard</p>
+        </NavLink>
+
+        <NavLink className="navlink" to="/" >
+        <img src="./Images/Events.svg" alt="Ticket" />
+        <p>Events</p>
+        </NavLink>
+
+        <NavLink className="navlink" to="/" >
+        <img src="./Images/Events.svg" alt="Ticket" />
+        <p>Bookings</p>
+        </NavLink>
+
+        <NavLink className="navlink" to="/" >
+        <img src="./Images/Events.svg" alt="Ticket" />
+        <p>Gallery</p>
+        </NavLink>
+
+        <NavLink className="navlink" to="/" >
+        <img src="./Images/Events.svg" alt="Ticket" />
+        <p>Feedback</p>
+        </NavLink>  
     </nav>
   )
 }
